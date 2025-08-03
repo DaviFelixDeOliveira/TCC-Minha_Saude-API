43615df1cfa99dc388626d97569b4de52bff428c
eduardosaraujo1 <eduardosaraujo1@proton.me>
2025-08-03 18:23:17 -0300

    feat: criado model, form validator e controller de cada objeto no banco

---
+++ app/Http/Controllers/CompartilhamentoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCompartilhamentoRequest;
+use App\Http\Requests\UpdateCompartilhamentoRequest;
+use App\Models\Compartilhamento;
+
+class CompartilhamentoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompartilhamentoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Compartilhamento $compartilhamento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Compartilhamento $compartilhamento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompartilhamentoRequest $request, Compartilhamento $compartilhamento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Compartilhamento $compartilhamento)
+    {
+        //
+    }
+}
